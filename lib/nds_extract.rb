581d25c8763d90bdeb71b0ce6f7e8e927173baf6
Kelly Powers <kpowers.nj@gmail.com>
2020-01-05 20:37:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -50,7 +50,7 @@ def total_gross(source)
   index = 0 
   while index < list_of_directors(source).length do 
     name = list_of_directors(source)[index]
-    total += source[index]
+    total += directors_totals[]
     index += 1 
   end 
   total 
