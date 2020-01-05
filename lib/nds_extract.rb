9b4575f7d3c8205059d5768074ea3abc664fde29
Kelly Powers <kpowers.nj@gmail.com>
2020-01-05 20:12:07 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -26,6 +26,14 @@ end
 
 def list_of_directors(source)
   # Write this implementation
+  result = []
+  director_index = 0 
+  while director_index < source.size do
+    director = source[director_index]
+    result.push[director[:name]]
+    director_index +=1 
+  end 
+  result
 end
 
 def total_gross(source)
@@ -38,6 +46,8 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  
+  
 end
 
 
